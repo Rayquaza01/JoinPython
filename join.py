7e9b82b1f2b44a15665ead3afd7877720d44cd12
Joe Jarvis <joe.jarvis109@gmail.com>
2016-11-25 15:12:15 -0500

    Rearranged code to happen in a more logical order

--- join.py
+++ join.py
@@ -11,33 +11,33 @@ ap.add_argument('-v','--vibration',help='Vibration for when the notification is 
 ap.add_argument('-u','--url',help='URL')
 ap.add_argument('-c','--clipboard',help='Clipboard',nargs='*')
 ap.add_argument('-f','--file',help='File (must be a publicly accessible URL)')
-ap.add_argument('-d','--device',help='The device name or a group (group.all, group.android, group.chrome, group.windows10, group.phone, group.tablet, group.pc)',nargs='*')
+ap.add_argument('-d','--device',help='The device name or a group (group.all, group.android, group.chrome, group.windows10, group.phone, group.tablet, group.pc)',nargs='+')
 ap.add_argument('-smsn','--smsnumber',help='Phone number to send an SMS to. If you want to set an SMS you need to set this and the smstext values',nargs='*')
 ap.add_argument('-smst','--smstext',help='Some text to send in an SMS. If you want to set an SMS you need to set this and the smsnumber values',nargs='*')
 ap.add_argument('-fi','--find',help='Set to true to make your device ring loudly')
 ap.add_argument('-w','--wallpaper',help='A publicly accessible URL of an image file. Will set the wallpaper on the receiving device')
 opts = ap.parse_args()
-print(opts.device)
 ##### Argument Parser ends here #####
-argsdict = {'text': opts.text, 'title': opts.title, 'icon': opts.icon, 'smallicon': opts.smallicon, 'priority': opts.priority, 'vibration': opts.vibration, 'url': opts.url, 'clipboard': opts.clipboard, 'file': opts.file, 'smsnumber': opts.smsnumber, 'smstext': opts.smstext, 'find': opts.find, 'wallpaper': opts.wallpaper, 'device': opts.device} #puts args into a dictionary for more convinient use
-for key, value in argsdict.items(): #curcumvents the need to encase args in quotes
-    if type(value) is list:
-        argsdict[key] = ' '.join(value)
-    if type(value) is str:
-        argsdict[key] = value
-try: #loads device json into a dictionary
-    with open('devices.json','r') as device:
-        deviceData = json.loads(device.read())
-except:
-    print('Could not read devices. Please run setup.py')
-if opts.smsnumber is not None:
-    try: #loads the contacts json as a dictionary if a number or name is supplied.
-        with open('contacts.json','r') as contact:
-            contactData = json.loads(contact.read())
-        argsdict['smsnumber'] = contactData[argsdict['smsnumber']] #replaces the name with the number
-    except: #defaults to a number if a name is not found in the json
-        print('No contact names found. Using numbers. Run setup.py for instructions on using names.')
 if opts.device is not None: #main process
+    try: #loads device json into a dictionary
+        with open('devices.json','r') as device:
+            deviceData = json.loads(device.read())
+    except:
+        print('Could not read devices. Please run setup.py')
+        exit()
+    argsdict = {'text': opts.text, 'title': opts.title, 'icon': opts.icon, 'smallicon': opts.smallicon, 'priority': opts.priority, 'vibration': opts.vibration, 'url': opts.url, 'clipboard': opts.clipboard, 'file': opts.file, 'smsnumber': opts.smsnumber, 'smstext': opts.smstext, 'find': opts.find, 'wallpaper': opts.wallpaper, 'device': opts.device} #puts args into a dictionary for more convinient use
+    for key, value in argsdict.items(): #curcumvents the need to encase args in quotes
+        if type(value) is list:
+            argsdict[key] = ' '.join(value)
+        if type(value) is str:
+            argsdict[key] = value
+    if opts.smsnumber is not None:
+        try: #loads the contacts json as a dictionary if a number or name is supplied.
+            with open('contacts.json','r') as contact:
+                contactData = json.loads(contact.read())
+            argsdict['smsnumber'] = contactData[argsdict['smsnumber']] #replaces the name with the number
+        except: #defaults to a number if a name is not found in the json
+            print('No contact names found. Assuming smsn is a number. Run setup.py for instructions on using names.')
     deviceName = argsdict['device']
     argsdict.pop('device',None) #removes device from argsdict to prevent sending extra params
     deviceIds = []
