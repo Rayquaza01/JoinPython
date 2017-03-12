f49c2cea883fb83bc8da7a5e0acb9bd8f670e57d
Joe Jarvis <joe.jarvis109@gmail.com>
2017-03-12 18:44:43 -0400

    More compliant with PEP8

--- join.py
+++ join.py
@@ -1,56 +1,82 @@
 #!/usr/bin/env python3
-import argparse, urllib.request, urllib.parse, json, os, re
-os.chdir(os.path.dirname(__file__)) # sets a constant working dir
-##### Argument Parser starts here #####
+import argparse
+import urllib.request
+import urllib.parse
+import json
+import os
+import re
+os.chdir(os.path.dirname(__file__))   # sets a constant working dir
+# Argument Parser starts here
 ap = argparse.ArgumentParser()
-ap.add_argument('-te','--text',help='Text (Tasker Command or notification text)',nargs='*')
-ap.add_argument('-ti','--title',help='Title (If set will create notification)',nargs='*')
-ap.add_argument('-i','--icon',help='Icon URI (publicly accessible URL or local file URI; used whenever a notification is created)')
-ap.add_argument('-s','--smallicon',help='Icon URI to be used as the statusbar icon')
-ap.add_argument('-p','--priority',help='Priority of the notification from -2 (lowest) to 2 (highest and default)',type=int,choices=range(-2,3))
-ap.add_argument('-v','--vibration',help='Vibration for when the notification is recived. Generate the pattern at http://autoremotejoaomgcd.appspot.com/AutoRemoteNotification.html')
-ap.add_argument('-u','--url',help='URL')
-ap.add_argument('-c','--clipboard',help='Clipboard',nargs='*')
-ap.add_argument('-f','--file',help='File (must be a publicly accessible URL)')
-ap.add_argument('-d','--device',help='The device name or a group (group.all, group.android, group.chrome, group.windows10, group.phone, group.tablet, group.pc)',nargs='*')
-ap.add_argument('-smsn','--smsnumber',help='Phone number to send an SMS to. If you want to set an SMS you need to set this and the smstext values',nargs='*')
-ap.add_argument('-cn','--callnumber',nargs='*',help='A number to call')
-ap.add_argument('-smst','--smstext',help='Some text to send in an SMS. If you want to set an SMS you need to set this and the smsnumber values',nargs='*')
-ap.add_argument('-fi','--find',help='Set to true to make your device ring loudly')
-ap.add_argument('-w','--wallpaper',help='A publicly accessible URL of an image file. Will set the wallpaper on the receiving device')
-ap.add_argument('-mms','--mmsfile',help='MMS file. smsnumber must be set for this to have an affect')
-ap.add_argument('-lw','--lockWallpaper',help='The wallpaper to set on the lockscreen (Android 7+), must be publicly accessible URL')
+ap.add_argument('-te', '--text', help='Text (Tasker Command or notification '
+                'text)', nargs='*')
+ap.add_argument('-ti', '--title', help='Title (If set will create '
+                'notification)', nargs='*')
+ap.add_argument('-i', '--icon', help='Icon URI (publicly accessible URL or '
+                'local file URI; used whenever a notification is created)')
+ap.add_argument('-s', '--smallicon', help='Icon URI to be used as the '
+                'statusbar icon')
+ap.add_argument('-p', '--priority', help='Priority of the notification from '
+                '-2 (lowest) to 2 (highest and default)', type=int,
+                choices=range(-2, 3))
+ap.add_argument('-v', '--vibration', help='Vibration for when the '
+                'notification is recived. Generate the pattern at '
+                'http://autoremotejoaomgcd.appspot.com/AutoRemoteNotification.'
+                'html')
+ap.add_argument('-u', '--url', help='URL')
+ap.add_argument('-c', '--clipboard', help='Clipboard', nargs='*')
+ap.add_argument('-f', '--file', help='File (must be a publicly accessible '
+                'URL)')
+ap.add_argument('-d', '--device', help='The device name or a group (group.'
+                'all, group.android, group.chrome, group.windows10, group.'
+                'phone, group.tablet, group.pc)', nargs='*')
+ap.add_argument('-smsn', '--smsnumber', help='Phone number to send an SMS to. '
+                'If you want to set an SMS you need to set this and the '
+                'smstext values', nargs='*')
+ap.add_argument('-cn', '--callnumber', nargs='*', help='A number to call')
+ap.add_argument('-smst', '--smstext', help='Some text to send in an SMS. If '
+                'you want to set an SMS you need to set this and the '
+                'smsnumber values', nargs='*')
+ap.add_argument('-fi', '--find', help='Set to true to make your device ring '
+                'loudly')
+ap.add_argument('-w', '--wallpaper', help='A publicly accessible URL of an '
+                'image file. Will set the wallpaper on the receiving device')
+ap.add_argument('-mms', '--mmsfile', help='MMS file. smsnumber must be set '
+                'for this to have an affect')
+ap.add_argument('-lw', '--lockWallpaper', help='The wallpaper to set on the '
+                'lockscreen (Android 7+), must be publicly accessible URL')
 opts = ap.parse_args()
-##### Argument Parser ends here #####
-try: # loads device json into a dictionary
-    with open('devices.json','r') as device:
+# Argument Parser ends here
+try:  # loads device json into a dictionary
+    with open('devices.json', 'r') as device:
         deviceData = json.loads(device.read())
 except:
     os.system('joinsetup.py -d')
-    with open('devices.json','r') as device:
+    with open('devices.json', 'r') as device:
         deviceData = json.loads(device.read())
-argsDict = vars(opts) # puts args into a dictionary for more convinient use
+argsDict = vars(opts)  # puts args into a dictionary for more convinient use
 if argsDict['device'] is None:
     argsDict['device'] = deviceData['pref']
-for key, value in argsDict.items(): # curcumvents the need to encase args in quotes
+for key, value in argsDict.items():  # fixes the need to encase args in quotes
     if type(value) is list:
         argsDict[key] = ' '.join(value)
 if opts.smsnumber or opts.callnumber is not None:
-    try: # loads the contacts json as a dictionary if a number or name is supplied.
-        with open('contacts.json','r') as contact:
+    try:  # loads the contacts json if a number or name is supplied.
+        with open('contacts.json', 'r') as contact:
             contactData = json.loads(contact.read())
-    except: # defaults to a number if a name is not found in the json
+    except:  # defaults to a number if a name is not found in the json
         pass
 if opts.smsnumber is not None:
     if argsDict['smsnumber'] in contactData:
-        argsDict['smsnumber'] = contactData[argsDict['smsnumber']] # replaces the name with the number
+        argsDict['smsnumber'] = contactData[argsDict['smsnumber']]
     else:
         clean = re.sub('[^\d+]', '', argsDict['smsnumber'])
         if re.match('\+?\d{10,}', clean):
             argsDict['smsnumber'] = clean
         else:
             print('Not a valid name or number. SMS cancelled.')
-            print('Make sure the number is valid or, if a name was used, run joinsetup.py -c to set up name support.')
+            print('Make sure the number is valid or, if a name was used, run '
+                  'joinsetup.py -c to set up name support.')
             argsDict['smsnumber'] = None
             argsDict['smstext'] = None
 if opts.callnumber is not None:
@@ -62,25 +88,22 @@ if opts.callnumber is not None:
             argsDict['callnumber'] = clean
         else:
             print('Not a valid name or number. Phone call canceled.')
-            print('Make sure the number is valid or, if a name was used, run joinsetup.py -c to set up name support.')
+            print('Make sure the number is valid or, if a name was used, run '
+                  'joinsetup.py -c to set up name support.')
             argsDict['callnumber'] = None
 deviceName = argsDict['device']
-argsDict.pop('device',None) # removes device from argsDict to prevent sending extra params
-argsDict['apikey'] = deviceData['apikey'] # https://plus.google.com/+Jo%C3%A3oDias/posts/GYwEvtSb238
-if ',' in deviceName: # allows for multiple device names separated by commas
+argsDict.pop('device', None)  # removes device to prevent sending extra params
+argsDict['apikey'] = deviceData['apikey']
+# https://plus.google.com/+Jo%C3%A3oDias/posts/GYwEvtSb238
+if ',' in deviceName:  # allows for multiple device names separated by commas
     argsDict['deviceNames'] = deviceName
-    # Old device Id multiple device code. Now uses beta device name + api key.
-    # deviceIds = []
-    # deviceNames = deviceName.split(',')
-    # for x in deviceNames:
-    #     deviceIds.append(deviceData[x])
-    # argsDict['deviceIds'] = ','.join('deviceIds')
-elif 'group' in deviceName: # allows for groups (group.android, etc.)
+elif 'group' in deviceName:  # allows for groups (group.android, etc.)
     argsDict['deviceId'] = deviceName
-else: # allows for single device
+else:  # allows for single device
     argsDict['deviceId'] = deviceData[deviceName]
 encoded = []
 for key, value in argsDict.items():
     if value is not None:
         encoded.append('='.join([key, urllib.parse.quote_plus(value)]))
-urllib.request.urlopen('https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?' + '&'.join(encoded))
+urllib.request.urlopen('https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/'
+                       'sendPush?' + '&'.join(encoded))
--- joinsetup.py
+++ joinsetup.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
-import urllib.request, json, csv, os, argparse, re
-os.chdir(os.path.dirname(__file__)) # sets a constant working dir
+import urllib.request
+import json
+import csv
+import os
+import argparse
+os.chdir(os.path.dirname(__file__))  # sets a constant working dir
 ap = argparse.ArgumentParser()
-ap.add_argument('-d','--devices',nargs='*')
-ap.add_argument('-c','--contacts',nargs='*')
-ap.add_argument('-f','--firstrun',nargs='*')
+ap.add_argument('-d', '--devices',  nargs='*')
+ap.add_argument('-c', '--contacts', nargs='*')
+ap.add_argument('-f', '--firstrun', nargs='*')
 opts = ap.parse_args()
 if opts.devices is None and opts.contacts is None and opts.firstrun is None:
     runAll = True
 else:
     runAll = False
 try:
-    with open('devices.json','r') as deviceJSON:
+    with open('devices.json', 'r') as deviceJSON:
         deviceDataOld = json.loads(deviceJSON.read())
         apikeyOld = deviceDataOld['apikey']
         prefOld = deviceDataOld['pref']
@@ -19,44 +23,52 @@ except:
     pass
 if opts.devices is not None or runAll is True:
     print('Devices Setup')
-    print('An API key is needed. Get your key at https://joinjoaomgcd.appspot.com/')
+    print('An API key is needed. Get your key at '
+          'https://joinjoaomgcd.appspot.com/')
     apikey = input('Enter your key (leave blank to reuse pre-existing key): ')
-    if apikey == '': # allows for using a pre-existing key
+    if apikey == '':  # allows for using a pre-existing key
         apikey = apikeyOld
-    devices = urllib.request.urlopen('https://joinjoaomgcd.appspot.com/_ah/api/registration/v1/listDevices?apikey=' + apikey + '').read()
+    devices = urllib.request.urlopen('https://joinjoaomgcd.appspot.com/_ah/api'
+                                     '/registration/v1/listDevices?apikey=' +
+                                     apikey).read()
     data = json.loads(devices.decode('utf-8'))
     deviceData = {}
     deviceData['apikey'] = apikey
-    for x in data['records']: # converts json to dict to simplify it
+    for x in data['records']:  # converts json to dict to simplify it
         deviceData[x['deviceName']] = x['deviceId']
         print(x['deviceName'])
-    pref = input('Choose the device name that you want to push to if no device is defined: ')
+    pref = input('Choose the device name that you want to push to if no '
+                 'device is defined: ')
     if pref == '':
         pref = prefOld
     deviceData['pref'] = pref
-    data = json.dumps(deviceData, sort_keys=True, indent=4) # convert back to json and write to file
-    with open('devices.json','w') as f:
+    data = json.dumps(deviceData, sort_keys=True, indent=4)  # write to file
+    with open('devices.json', 'w') as f:
         f.write(str(data))
     print('Sucessfully saved device data to devices.json!')
     print('')
 if opts.contacts is not None or runAll:
     print('Contacts Setup')
-    print('Export your google contacts csv file into the working directory as google.csv')
-    print('https://www.google.com/contacts/u/0/?cplus=0# contacts is where you can export the file.')
+    print('Export your google contacts csv file into the working directory as '
+          'google.csv')
+    print('https://www.google.com/contacts/u/0/?cplus=0# contacts is where you'
+          'can export the file.')
     print('Export the file in the Google format.')
-    input('Press enter when this is done or close the window to cancel contacts setup')
+    input('Press enter when this is done or close the window to cancel '
+          'contacts setup')
     contactsData = {}
-    with open('google.csv','r') as contacts: # converts csv to dict to simplify it
+    with open('google.csv', 'r') as contacts:
         read = csv.reader(contacts)
         for row in read:
             if row[31] == 'Mobile':
                 contactsData[row[0]] = row[32]
             if row[33] == 'Mobile':
                 contactsData[row[0]] = row[34]
-    data = json.dumps(contactsData, sort_keys=True, indent=4) # convert to json and write to file
-    with open('contacts.json','w') as f:
+    data = json.dumps(contactsData, sort_keys=True, indent=4)  # write to file
+    with open('contacts.json', 'w') as f:
         f.write(str(data))
-    print('Sucessfully saved contacts data to contacts.json! You can delete google.csv')
+    print('Sucessfully saved contacts data to contacts.json! '
+          'You can delete google.csv')
     print('')
 print('For more instructions, view the readme.')
 print('This concludes the setup.')
