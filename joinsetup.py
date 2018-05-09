2578bec05cd7e0ceea9c88c312b90363808ccf0f
Joe Jarvis <joe.jarvis109@gmail.com>
2018-05-09 16:15:51 -0400

    Started splitting into separate module for request to simplify main script

--- joinsetup.py
+++ joinsetup.py
@@ -6,65 +6,65 @@ import os
 import argparse
 os.chdir(os.path.dirname(__file__))  # sets a constant working dir
 ap = argparse.ArgumentParser()
-ap.add_argument('-d', '--devices',  nargs='*')
-ap.add_argument('-c', '--contacts', nargs='*')
+ap.add_argument("-d", "--devices",  nargs="*")
+ap.add_argument("-c", "--contacts", nargs="*")
 opts = ap.parse_args()
 try:
-    with open('devices.json', 'r') as deviceJSON:
+    with open("devices.json", "r") as deviceJSON:
         deviceDataOld = json.loads(deviceJSON.read())
-        apikeyOld = deviceDataOld['apikey']
-        prefOld = deviceDataOld['pref']
+        apikeyOld = deviceDataOld["apikey"]
+        prefOld = deviceDataOld["pref"]
 except:
     pass
 if opts.devices is not None:
-    print('Devices Setup')
-    print('An API key is needed. Get your key at '
-          'https://joinjoaomgcd.appspot.com/')
-    apikey = input('Enter your key (leave blank to reuse pre-existing key): ')
-    if apikey == '':  # allows for using a pre-existing key
+    print("Devices Setup")
+    print("An API key is needed. Get your key at "
+          "https://joinjoaomgcd.appspot.com/")
+    apikey = input("Enter your key (leave blank to reuse pre-existing key): ")
+    if apikey == "":  # allows for using a pre-existing key
         apikey = apikeyOld
-    devices = urllib.request.urlopen('https://joinjoaomgcd.appspot.com/_ah/api'
-                                     '/registration/v1/listDevices?apikey=' +
+    devices = urllib.request.urlopen("https://joinjoaomgcd.appspot.com/_ah/api"
+                                     "/registration/v1/listDevices?apikey=" +
                                      apikey).read()
-    data = json.loads(devices.decode('utf-8'))
+    data = json.loads(devices.decode("utf-8"))
     deviceData = {}
-    deviceData['apikey'] = apikey
-    for x in data['records']:  # converts json to dict to simplify it
-        deviceData[x['deviceName']] = x['deviceId']
-        print(x['deviceName'])
-    pref = input('Choose the device name that you want to push to if no '
-                 'device is defined: ')
-    if pref == '':
+    deviceData["apikey"] = apikey
+    for x in data["records"]:  # converts json to dict to simplify it
+        deviceData[x["deviceName"]] = x["deviceId"]
+        print(x["deviceName"])
+    pref = input("Choose the device name that you want to push to if no "
+                 "device is defined: ")
+    if pref == "":
         pref = prefOld
-    deviceData['pref'] = pref
+    deviceData["pref"] = pref
     data = json.dumps(deviceData, sort_keys=True, indent=4)  # write to file
-    with open('devices.json', 'w') as f:
+    with open("devices.json", "w") as f:
         f.write(str(data))
-    print('Sucessfully saved device data to devices.json!')
-    print('')
+    print("Sucessfully saved device data to devices.json!")
+    print("")
 if opts.contacts is not None:
-    print('Contacts Setup')
-    print('Export your google contacts csv file into the working directory as '
-          'google.csv')
-    print('https://www.google.com/contacts/u/0/?cplus=0# contacts is where you'
-          'can export the file.')
-    print('Export the file in the Google format.')
-    input('Press enter when this is done or close the window to cancel '
-          'contacts setup')
+    print("Contacts Setup")
+    print("Export your google contacts csv file into the working directory as "
+          "google.csv")
+    print("https://www.google.com/contacts/u/0/?cplus=0# contacts is where you"
+          "can export the file.")
+    print("Export the file in the Google format.")
+    input("Press enter when this is done or close the window to cancel "
+          "contacts setup")
     contactsData = {}
-    with open('google.csv', 'r') as contacts:
+    with open("google.csv", "r") as contacts:
         read = csv.reader(contacts)
         for row in read:
-            if row[31] == 'Mobile':
+            if row[31] == "Mobile":
                 contactsData[row[0]] = row[32]
-            if row[33] == 'Mobile':
+            if row[33] == "Mobile":
                 contactsData[row[0]] = row[34]
     data = json.dumps(contactsData, sort_keys=True, indent=4)  # write to file
-    with open('contacts.json', 'w') as f:
+    with open("contacts.json", "w") as f:
         f.write(str(data))
-    print('Sucessfully saved contacts data to contacts.json! '
-          'You can delete google.csv')
-    print('')
-print('For more instructions, view the readme.')
-print('This concludes the setup.')
-input('Press enter or close the window.')
+    print("Sucessfully saved contacts data to contacts.json! "
+          "You can delete google.csv")
+    print("")
+print("For more instructions, view the readme.")
+print("This concludes the setup.")
+input("Press enter or close the window.")
